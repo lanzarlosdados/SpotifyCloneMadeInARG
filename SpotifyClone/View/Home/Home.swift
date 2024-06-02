1a7492dffcb59671eca12af34acc85e4fa10d51b
Fabian Zarate <56480007+lanzarlosdados@users.noreply.github.com>
2024-06-02 11:54:18 -0300

    Refactor list in home

--- SpotifyClone/View/Home/Home.swift
+++ SpotifyClone/View/Home/Home.swift
@@ -10,14 +10,16 @@ struct Home: View {
     let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 1)
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: columns, spacing: 20) {
+        NavigationStack {
+            List {
                 ForEach(1..<6) { number in
                     SectionViewHome()
+                        .listRowSeparator(.hidden)
+                        .listRowInsets(.init(top: 0 , leading: 0, bottom: 0, trailing: 0))
                 }
             }
+            .listStyle(.plain)
         }
-        .scrollIndicators(.hidden)
     }
 }
 
