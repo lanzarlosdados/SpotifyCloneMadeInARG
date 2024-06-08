67dc9f200dbde9dc3e6c332da740bf5eeac33cbd
Fabian Zarate <56480007+lanzarlosdados@users.noreply.github.com>
2024-06-08 19:51:55 -0300

    create container grid card view search

---
+++ SpotifyClone/View/SearchScreen/ContainerViewSearchGrid.swift
@@ -0,0 +1,18 @@
+//
+//  ContainerViewSearchGrid.swift
+//  SpotifyClone
+//
+//  Created by fabian zarate on 08/06/2024.
+//
+
+import SwiftUI
+
+struct ContainerViewSearchGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContainerViewSearchGrid()
+}
